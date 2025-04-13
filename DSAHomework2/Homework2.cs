64643f9cf774e8c14bb24586dc5688845a57fe2a
JeremyE-Dev <eudaly_19@hotmail.com>
2025-04-12 21:27:14 -0500

    finish homework 2

--- DSAHomework2/Homework2.cs
+++ DSAHomework2/Homework2.cs
@@ -5,6 +5,7 @@ using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Globalization;
+using System.Transactions;
 
 namespace DSAHomework2
 {
@@ -16,59 +17,50 @@ namespace DSAHomework2
             SinglyLinkedList<double> polynomial = new SinglyLinkedList<double>();
             foreach (double coefficient in coefficients)
             {
-                // Append each coefficient to the polynomial
                 AppendTerm(polynomial, coefficient);
             }
 
             return polynomial;
         }
-
-        public static void printCoefficients(SinglyLinkedList<double> polynomial)
-        {
-            // Print the coefficients of the polynomial
-            SinglyLinkedList<double>.Element current = polynomial.Head;
-            while (current != null)
-            {
-                Console.Write(current.Data + " ");
-                current = current.Next;
-            }
-            Console.WriteLine();
-        }
+    
 
         //a) Implement a method called AppendTerm
         // This method should append (insert at the end) the value "coefficient" to "polynomial"
         // For example, appending 3.1 to polynomial already containing 6.0 > 0.0 > -5.3
         // should result in the value 3.1 being added at the end: 6.0 > 0.0 > -5.3 > 3.1
 
+        //** Assembling the polynomoial **
         // the entity assembling the polynomial, needs to know how to represent the missing terms
         // and that we are using standard form for the polynomial
         // so the terms are being implemented manually in the list
+
+        // a "Term" is a coefficient, a variable, and an exponent
+
+        // the position will determine the power, which will come into play when evaluating and displaying
+
+        //** Examples and Test Cases
+
+        // Test case 1:  x + 1.0 with x = 1.0
+        // -- in this case if there is no coefficient but the term exists then, use 1 as a placeholder
+        // -- because x^1 = 1.0x^1
+        // -- list would be represented as 1.0 > 1.0
+
+        // Test case 2: x^2 - 1.0 with x = 2.03
+        // -- in this case if there is no coefficient but the term exists then, use 1 as a placeholder
+        // -- list would be represented as 1.0 > 0.0 > -1.0
+        // -- the 0.0 represents the x^1 term, which is not present in this case
+
+        // Test case 3:  -3.0x^3 + 0.5x^2 - 2.0x
+        // -- list would be represented as -3.0 > 0.5 > -2.0 > 0.0
+        // -- the 0.0 represents the constant term
+
+        // Test case 4: -0.3125x^4 - 9.915x^2 - 7.75x - 40.0 with x = 123.45
+        // -- list would be represented as -0.3125 > 0.0 > -9.915 > -7.75 > -40.0
+        // -- 0.0 is the placeholder for the x^3 term, which is not present in this case
+
+
         public static void AppendTerm(SinglyLinkedList<double> polynomial, double coefficient)
         {
-            // examples:
-            // Test case 1:  x + 1.0 with x = 1.0
-            // -- in this case if there is no coefficient but the term exists then, use 1 as a placeholder
-            // -- because x^1 = 1.0x^1
-            // -- list would be represented as 1.0 > 1.0
-
-            // Test case 2: x^2 - 1.0 with x = 2.03
-            // -- in this case if there is no coefficient but the term exists then, use 1 as a placeholder
-            // -- list would be represented as 1.0 > 0.0 > -1.0
-            // -- the 0.0 represents the x^1 term, which is not present in this case
-
-            // Test case 3:  -3.0x^3 + 0.5x^2 - 2.0x
-            // -- list would be represented as -3.0 > 0.5 > -2.0 > 0.0
-            // -- the 0.0 represents the constant term
-
-            // Test case 4: -0.3125x^4 - 9.915x^2 - 7.75x - 40.0 with x = 123.45
-            // -- list would be represented as -0.3125 > 0.0 > -9.915 > -7.75 > -40.0
-            // -- 0.0 is the placeholder for the x^3 term, which is not present in this case
-
-
-            // the position will determine the power, which will come into play when evaluating and diplaying
-            // a "Term" is a coefficient, a variable, and an exponent
-
-
             // Check if the polynomial is empty
             if (polynomial.Empty)
             {
@@ -87,6 +79,12 @@ namespace DSAHomework2
         // for example, displaying polynomial 6.0 > 0.0 > -5.3 > 3.1 should
         // return 6.0x^3 - 5.3x + 3.1 being printed.
 
+        // Test cases:
+        // Test case 1:  x + 1.0 with 
+        // Test case 2: x^2 - 1.0 with x = 2.03
+        // Test case 3:  -3.0x^3 + 0.5x^2 - 2.0x
+        // Test case 4: -0.3125x^4 - 9.915x^2 - 7.75x - 40.0 with x = 123.45
+
         public static void Display(SinglyLinkedList<double> polynomial)
         {
             // Test case 1:  x + 1.0 with x = 1.0
@@ -94,17 +92,12 @@ namespace DSAHomework2
             // Test case 3:  -3.0x^3 + 0.5x^2 - 2.0x
             // Test case 4: -0.3125x^4 - 9.915x^2 - 7.75x - 40.0 with x = 123.45
 
-            // if the number is whole then print
+            // all test cases have at least  2 terms, so there will always be one "x"
 
-            // print the coefficients
             SinglyLinkedList<double>.Element current = polynomial.Head;
             int exponent = polynomial.Size - 1;
             while (current != null && exponent >= 0)
             {
-                // all test cases have at least  2 terms, so there will always be one "x"
-                // position relative to size
-                // is the term zero?
-
                 // if the term is zero it is a placeholder, skip it, but decrement the exponent
                 if (current.Data == 0.0)
                 {
@@ -113,9 +106,8 @@ namespace DSAHomework2
                     continue;
                 }
 
-                // 1 means no coefficient, but there is a variable, just print the "x" and the exponent if there is one
-                // this is a special or placeholder case where the coefficient is 1.0 used to indicate that ther is 
-                // no calcuable coefficient
+                // 1 means either there no coefficient, but there is a variable and just print the "x" and the exponent (if there is one)
+                // if the 1.0 is a constant then we print that
                 else if (current.Data == 1.0)
                 {
                     if (exponent > 1) // an exponent exists i.e. (x^3, x^2...)
@@ -128,7 +120,6 @@ namespace DSAHomework2
                     }
                     else
                     {
-                       
                         Console.Write($"{current.Data:0.0}");
                     }
 
@@ -144,8 +135,7 @@ namespace DSAHomework2
                                 IsWhole(current.Data)
                                     ? $"{current.Data:0.0}x^{exponent}"
                                     : $"{current.Data}x^{exponent}"
-                                );
-                            //IsWhole(current.Data) ? Console.Write($"{current.Data:0.0}" + "x" + "^" + exponent) : Console.Write(current.Data + "x" + "^" + exponent);
+                            );
 
                         } else
                         {
@@ -153,58 +143,55 @@ namespace DSAHomework2
                                IsWhole(current.Data)
                                    ? $"{Math.Abs(current.Data):0.0}x^{exponent}"
                                    : $"{Math.Abs(current.Data)}x^{exponent}"
-                               );
-
-                            // Console.Write(Math.Abs(current.Data) + "x" + "^" + exponent);
+                            );
                         }
                     }
                     else if (exponent == 1)
                     {
                         if (current == polynomial.Head && current.Data < 0)
                         {
-                            // Console.Write(current.Data + "x");
 
                             Console.Write(
                               IsWhole(current.Data)
                                   ? $"{current.Data:0.0}x"
                                   : $"{current.Data}x^"
-                              );
-
+                            );
 
                         } else
                         {
-                            // Console.Write(Math.Abs(current.Data) + "x");
                             Console.Write(
                              IsWhole(current.Data)
                                  ? $"{Math.Abs(current.Data):0.0}x"
                                  : $"{Math.Abs(current.Data)}x^"
                              );
                         }
-                          
                     }
                     else
                     {
                         if (current == polynomial.Head && current.Data < 0)
                         {
 
-                            Console.Write($"{current.Data:0.0}"); // Interpolated string with format
+                            Console.Write($"{current.Data:0.0}"); 
                         }
                         else
                         {
-                            Console.Write($"{Math.Abs(current.Data):0.0}"); // Interpolated string with format
+                            Console.Write($"{Math.Abs(current.Data):0.0}"); 
                         }
-
-                        ////Console.Write($"{current.Data:0.0}"); // Interpolated string with format
-                        //Console.Write($"{Math.Abs(current.Data):0.0}"); // Interpolated string with format
                     }
                 }
-
-                // if the term is not the last one
-                //this deals with the sign after the current term given term
+                
+                // Handle the  the sign (+/-) after the current term
+                // if (current.Next != null && current.Next.Data != 0 && exponent != 1)
                 if (current.Next != null)
                 {
+                    // edge case if the last term is a 0.0 placeholder
+                    // do not want a +/- sign in that case
+                    if (current.Next == polynomial.Tail && current.Next.Data == 0 && exponent == 1)
+                    {
+
+                    }
                     // check the sign of the next term
-                    if (current.Next.Data > 0)
+                    else if (current.Next.Data > 0)
                     {
                         Console.Write(" + ");
                     }
@@ -214,8 +201,6 @@ namespace DSAHomework2
                         current.Next.Data = Math.Abs(current.Next.Data);
                     }
                 }
-
-         
 
                 exponent--;
                 current = current.Next;
@@ -229,9 +214,42 @@ namespace DSAHomework2
         // and x having value 7.0 the function should return 2024.0
         // (The result of evaluating 6.0*7.0^3 - 5.3*7.0 + 3.1 )
 
-        public static double Evaluate(SinglyLinkedList<double> polynomial, double x)
+        // Test case 1:  x + 1.0 with x = 1.0
+        // Test case 2: x^2 - 1.0 with x = 2.03 
+        // Test case 3:  -3.0x^3 + 0.5x^2 - 2.0x with x = 05.0
+        // Test case 4: -0.3125x^4 - 9.915x^2 - 7.75x - 40.0 with x = 123.45
+
+        // Each element of the list represents the coefficient for x^(length of the list - position in list - 1). So the first element of the list above is the coefficient for x^(4 - 0 - 1 = 3).
+
+        public static double Evaluate(SinglyLinkedList<double> polynomial, double variable)
         {
-            return 0.0;
+            double result = 0.0;
+
+            // set the current term to head of polynomial
+            // This allows us to track the where we are in the linked list
+            SinglyLinkedList<double>.Element current = polynomial.Head;
+
+            //this is the location of the current term in the polynomial
+            // acts like an index, so we can calculate the exponent
+            int position = 0;
+            double currentTerm = 0.0;
+
+            while (current != null)
+            {
+
+                if (current.Data == 0)
+                {
+                    current = current.Next;
+                    position++;
+                    continue;
+                }
+
+                currentTerm = Homework2.CalculateCurrentTerm(current.Data, position, polynomial.Size, variable);
+                result += currentTerm;
+                current = current.Next;
+                position++;
+            }
+            return result;
         }
 
         //Helper methods
@@ -240,5 +258,31 @@ namespace DSAHomework2
             return number % 1 == 0;
 
         }
+
+        private static double CalculateCurrentTerm(double coefficient,int position, int lengthOfList, double variable)
+        {
+            //calculate the exponent
+            // exponent will either be 4, 3, 2, 1, or 0
+            double result = 0.0; ;
+            int exponent = lengthOfList - position - 1;
+
+            double variableToExponent = Math.Pow(variable, exponent);
+            return result = coefficient * variableToExponent;
+
+        }
+
+        // Helper method for texting coefficients are listed correctly
+        private static void printCoefficients(SinglyLinkedList<double> polynomial)
+        {
+            // Print the coefficients of the polynomial
+            SinglyLinkedList<double>.Element current = polynomial.Head;
+            while (current != null)
+            {
+                Console.Write(current.Data + " ");
+                current = current.Next;
+            }
+            Console.WriteLine();
+        }
+
     }
 }
--- DSAHomework2/Program.cs
+++ DSAHomework2/Program.cs
@@ -6,17 +6,8 @@ namespace DSAHomework2
         static void Main(string[] args)
         {
 
-
-            // Homework2.printCoefficients(polynomial);
-
-
-
-            //write a program to test the method parts a - c. Your test program should demonstrate
-            //creating, displaying, and evaluating the following polynomials with the given values for x:
-
-            // Test case 1:  x + 1.0 with x = 1.0
-
             // ** Set up terms **
+            
             //  x + 1.0
             double[] coefficients_1 = { 1.0, 1.0};
             
@@ -24,18 +15,27 @@ namespace DSAHomework2
             double[] coefficients_2 = { 1.0, 0.0, -1.0};
             
             // -3.0x^3 + 0.5x^2 - 2.0x
-            double[] coefficients_3 = { -3.0, 0.5, -2.0};
+            double[] coefficients_3 = { -3.0, 0.5, -2.0, 0.0};
             
             // -0.3125x^4 - 9.915x^2 - 7.75x - 40.0
             double[] coefficients_4 = { -.3125, 0.0, -9.915, -7.75, -40.0};
 
-            // ** Create Lists of coefficients**
+            // ** Create lists of coefficients **
+            
             SinglyLinkedList<double> polynomial_1 = Homework2.Create(coefficients_1);
             SinglyLinkedList<double> polynomial_2 = Homework2.Create(coefficients_2);
             SinglyLinkedList<double> polynomial_3 = Homework2.Create(coefficients_3);
             SinglyLinkedList<double> polynomial_4 = Homework2.Create(coefficients_4);
 
-            // ** Display **
+            // ** Evaluate results **
+            
+            double result_1 = Homework2.Evaluate(polynomial_1, 1.0);
+            double result_2 = Homework2.Evaluate(polynomial_2, 2.03);
+            double result_3 = Homework2.Evaluate(polynomial_3, 5.0);
+            double result_4 = Homework2.Evaluate(polynomial_4, 123.45);
+
+            // ** Display polynomials **
+            
             Console.WriteLine("polynomial_1: should display x + 1.0 ");
             Homework2.Display(polynomial_1);
             Console.WriteLine();
@@ -50,28 +50,29 @@ namespace DSAHomework2
 
             Console.WriteLine("polynomial_4: should display -0.3125x^4 - 9.915x^2 - 7.75x - 40.0 ");
             Homework2.Display(polynomial_4);
+            Console.WriteLine();
 
+            // **  Print evaluated results **
 
-
-            // ** Evaluate **
+            // Test case 1:  x + 1.0 with x = 1.0
+            Console.WriteLine($"Expected Result of polynomial_1: 2.0");
+            Console.WriteLine($"Actual Result of polynomial_1: {result_1}");
+            Console.WriteLine();
 
             // Test case 2: x^2 - 1.0 with x = 2.03
+            Console.WriteLine($"Expected Result of polynomial_2:  3.1209");
+            Console.WriteLine($"Actual Result of polynomial_2: {result_2}");
+            Console.WriteLine();
 
-            // ** Create **
-            // ** Display **
-            // ** Evaluate **
-
-            //Test case 3:  -3.0x^3 + 0.5x^2 - 2.0x
-
-            // ** Create **
-            // ** Display **
-            // ** Evaluate **
+            //Test case 3:  -3.0x^3 + 0.5x^2 - 2.0x with x = 5.0
+            Console.WriteLine($"Expected Result of polynomial_3: -372.5");
+            Console.WriteLine($"Actual Result of polynomial_3: {result_3}");
+            Console.WriteLine();
 
             // Test case 4: -0.3125x^4 - 9.915x^2 - 7.75x - 40.0 with x = 123.45
-
-            // ** Create **
-            // ** Display **
-            // ** Evaluate **
+            Console.WriteLine($"Expect result of polynomial_4: -72,731,671.68625821");
+            Console.WriteLine($"Actual Result of polynomial_4: {result_4} ");
+            Console.WriteLine();
 
         }
     }
